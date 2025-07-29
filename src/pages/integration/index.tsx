89a39d8a76abe009509003a8c358621ec7b59e80
Kai4632 <Ahmednekoshhekdww@hotmail.com>
2025-07-29 09:06:43 +0100

    added the technical page

---
+++ src/pages/integration/index.tsx
@@ -0,0 +1,10 @@
+export default function Integration() {
+    return (
+      <div className="p-4 text-center">
+        <h1 className="text-3xl font-bold text-blue-600">About Page</h1>
+        <p className="mt-4">This is the about page of our application.</p>
+      </div>
+    )
+  }
+  
+  
