6ec62ceb264b27f60aa78f1286fd8c877cc640ed
Kai <Ahmednekoshhekdww@hotmail.com>
2025-07-28 20:41:28 -0400

    Update index.tsx

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
       <div className="p-4 text-center">
-        <h1 className="text-3xl font-bold text-blue-600">About Page</h1>
+        <h1 className="text-3xl font-bold text-blue-600">About Pag</h1>
         <p className="mt-4">This is the about page of our application.</p>
       </div>
     )
